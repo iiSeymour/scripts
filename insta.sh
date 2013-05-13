267bfed3c82b9783c5ec1002382ab0756e4b618f
Chris Seymour <chris.j.seymour@hotmail.com>
2013-05-14 00:12:19 +0200

    Adding insta.sh

---
+++ insta.sh
@@ -0,0 +1,26 @@
+#!/bin/bash                                                                                                                                            
+
+# Stash a user instagram photos on your drive
+
+if [ $# -eq 0 ] ; then
+    echo 'insta.sh [user] - stash which users pictures?'
+    exit 1
+fi
+
+page="$1"
+dir="${1}_images"
+
+if [ ! -d "$dir" ]; then
+    mkdir "$dir"
+fi
+
+if [ -f "${dir}/${page}" ]; then
+    rm "${dir}/${page}"
+fi
+
+wget "http://instagram.com/${page}" -P "${dir}"
+grep -Po 'http:\\/\\/\K([[:alnum:]]+[.])+com\\/[[:alnum:]]+_7[.]jpg' "${dir}/${page}" |
+tr -d '\\' |
+grep -v 'instagram' |
+xargs wget -P "$dir"
+
