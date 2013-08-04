4c6d6bf8299e4c37d1c340cc5f6fe0d4042b57c7
Chris Seymour <chris.j.seymour@hotmail.com>
2013-08-04 18:08:21 +0100

    Remove any backup images (+ added comments to script)

--- insta.sh
+++ insta.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 
 # Stash a user instagram photos on your drive
 
-if [ $# -eq 0 ] ; then
+if [ $# -eq 0 ]; then
     echo 'insta.sh [user] - stash which users pictures?'
     exit 1
 fi
@@ -14,12 +14,19 @@ if [ ! -d "$dir" ]; then
     mkdir "$dir"
 fi
 
+# get the users instagram page
+wget -q "http://instagram.com/${page}" -P "${dir}" -a "${dir}/insta.log"
+
+# parse the page and download the images
+grep -Po 'http:\\/\\/\K([[:alnum:]]+[.])+com\\/[[:alnum:]]+_7[.]jpg' "${dir}/${page}" |
+tr -d '\\' |
+grep -v 'instagram' |
+xargs wget -P "$dir" -a "${dir}/insta.log"
+
+# remove the page
 if [ -f "${dir}/${page}" ]; then
     rm "${dir}/${page}"
 fi
 
-wget "http://instagram.com/${page}" -P "${dir}"
-grep -Po 'http:\\/\\/\K([[:alnum:]]+[.])+com\\/[[:alnum:]]+_7[.]jpg' "${dir}/${page}" |
-tr -d '\\' |
-grep -v 'instagram' |
-xargs wget -P "$dir"
+# remove any backup images
+rm -f "${dir}/*.[0-9]"
